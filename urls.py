c22027ca838f4748447df353d94105d3f4517315
Juan Conde <juanmconde@outlook.com>
2024-12-16 20:38:25 -0300

    controlar no funciona muy bien

--- urls.py
+++ urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from maquinas import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('maquinas.urls')),  # Incluye todas las URLs de la app 'maquinas'
+    path('', views.lista_maquinas, name='inicio'),
+    path('registrar/', views.registrar_cliente, name='registrar_cliente'),
+    path('registrar_reparacion/', views.registrar_reparacion, name='registrar_reparacion'),
+    path('clientes/', views.lista_clientes, name='lista_clientes'),
+    path('maquinas/', include('maquinas.urls')),
 ]
