fa902f0fb68461c6d0e2c1aef43b81e48fee2d4b
Juan Conde <juanmconde@outlook.com>
2024-12-18 07:11:03 -0300

    agrege css

--- urls.py
+++ urls.py
@@ -1,12 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from maquinas import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.lista_maquinas, name='inicio'),
-    path('registrar/', views.registrar_cliente, name='registrar_cliente'),
-    path('registrar_reparacion/', views.registrar_reparacion, name='registrar_reparacion'),
-    path('clientes/', views.lista_clientes, name='lista_clientes'),
-    path('maquinas/', include('maquinas.urls')),
+    path('', include('maquinas.urls')),
 ]
